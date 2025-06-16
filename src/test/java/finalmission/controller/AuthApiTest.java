d05f628b6578d0939475b0523654cb5bb206e51a
kjungw <sagakkb@gmail.com>
2025-06-16 12:02:43 +0900

    refactor: 기존의 외부 API 호출 제거

--- src/test/java/finalmission/controller/AuthApiTest.java
+++ src/test/java/finalmission/controller/AuthApiTest.java
@@ -15,7 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class AuthControllerTest {
+class AuthApiTest {
 
     @LocalServerPort
     private int port;
