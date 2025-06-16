f9ec13003c571a4d78de3eeb13523b153917e932
kjungw <sagakkb@gmail.com>
2025-06-16 12:09:20 +0900

    test: 로그아웃 통합 테스트 추가

--- src/test/java/finalmission/controller/AuthApiTest.java
+++ src/test/java/finalmission/controller/AuthApiTest.java
@@ -8,11 +8,13 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class AuthApiTest {
@@ -28,35 +30,63 @@ class AuthApiTest {
         memberRepository.deleteAll();
     }
 
+    @Nested()
     @DisplayName("로그인 할 수 있다.")
-    @Test
-    void canLogin() {
-        memberRepository.save(new Member("member@email.com", "qwer1234!", "kim", Role.GENERAL));
+    public class canLogin {
 
-        RestAssured
-                .given().log().all()
-                .contentType(ContentType.JSON)
-                .port(port)
-                .body(new LoginRequest("member@email.com", "qwer1234!"))
-                .when().log().all()
-                .post("/login")
-                .then()
-                .statusCode(200);
+        @DisplayName("정상적으로 로그인 할 수 있다.")
+        @Test
+        void canLogin() {
+            // given
+            memberRepository.save(new Member("member@email.com", "qwer1234!", "kim", Role.GENERAL));
+
+            // when & then
+            RestAssured
+                    .given().log().all()
+                    .contentType(ContentType.JSON)
+                    .port(port)
+                    .body(new LoginRequest("member@email.com", "qwer1234!"))
+                    .when().log().all()
+                    .post("/login")
+                    .then()
+                    .statusCode(HttpStatus.OK.value());
+        }
+
+        @DisplayName("비밀번호가 맞지 않은 경우 로그인이 불가능하다.")
+        @Test
+        void cannotLogin() {
+            // given
+            memberRepository.save(new Member("member@email.com", "asdf1234!", "kim", Role.GENERAL));
+
+            // when & then
+            RestAssured
+                    .given().log().all()
+                    .contentType(ContentType.JSON)
+                    .port(port)
+                    .body(new LoginRequest("member@email.com", "qwer1234!"))
+                    .when().log().all()
+                    .post("/login")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
     }
 
-    @DisplayName("비밀번호가 맞지 않은 경우 로그인이 불가능하다.")
-    @Test
-    void cannotLogin() {
-        memberRepository.save(new Member("member@email.com", "asdf1234!", "kim", Role.GENERAL));
+    @Nested
+    @DisplayName("로그아웃 할 수 있다.")
+    public class canLogout {
 
-        RestAssured
-                .given().log().all()
-                .contentType(ContentType.JSON)
-                .port(port)
-                .body(new LoginRequest("member@email.com", "qwer1234!"))
-                .when().log().all()
-                .post("/login")
-                .then()
-                .statusCode(400);
+        @DisplayName("정상적으로 로그아웃 할 수 있다.")
+        @Test
+        void canLogout() {
+            // when & then
+            RestAssured
+                    .given().log().all()
+                    .contentType(ContentType.JSON)
+                    .port(port)
+                    .when().log().all()
+                    .post("/logout")
+                    .then()
+                    .statusCode(HttpStatus.NO_CONTENT.value());
+        }
     }
 }
